fc79cf715bf4faa38d1d65c5440b51b3162a7188
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-09 14:31:25 +0530

    file added

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,3 +1,13 @@
+# Sort the Colors
+class Solution:
+    def sortColors(self, nums):
+        n=len(nums)
+        for i in range(n):
+            for j in range(n-i-1):
+                if nums[j]>nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+        return nums
+    
 # Sort the array and count how many indices have different values compared to the original array.
 nums = [1,1,4,2,1,3]
 new_nums = nums[:]
