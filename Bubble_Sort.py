797c60641162920631a3f994f7927d08140d1c0b
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-09 14:30:19 +0530

    file added

---
+++ Bubble_Sort.py
@@ -0,0 +1,13 @@
+# Sort the array and count how many indices have different values compared to the original array.
+nums = [1,1,4,2,1,3]
+new_nums = nums[:]
+n = len(new_nums)
+for i in range(n):
+  for j in range(n-i-1):
+    if new_nums[j] > new_nums[j+1]:
+      new_nums[j], new_nums[j+1] = new_nums[j+1], new_nums[j]
+count=0
+for i in range(len(new_nums)):
+  if new_nums[i]!=nums[i]:
+    count+=1
+print(count)
