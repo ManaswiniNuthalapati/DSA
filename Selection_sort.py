7f2fbac144f9465e15a1ba4c5e09c27046ab0aed
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-15 10:00:00 +0530

    Work done

--- Selection_sort.py
+++ Selection_sort.py
@@ -10,3 +10,16 @@ def selection_sort(arr):
     return arr
 arr = [64, 25, 12, 22, 11]
 print(selection_sort(arr))
+
+# Sort in Descending Order
+def selection_sort_desc(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        max_index = i
+        for j in range(i + 1, n):
+            if arr[j] > arr[max_index]:
+                max_index = j
+        arr[i], arr[max_index] = arr[max_index], arr[i]
+    return arr
+arr = [64, 25, 12, 22, 11]
+print(selection_sort_desc(arr))
