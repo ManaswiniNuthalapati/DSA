7f18ac35235092416e162098d3394cb0eb7b9402
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-18 10:00:00 +0530

    Work done

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -11,3 +11,17 @@ def insertion_sort(arr):
     return arr
 arr = [12, 11, 13, 5, 6]
 print(insertion_sort(arr))
+
+# Sort in Descending Order
+def insertion_sort_desc(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+arr = [12, 11, 13, 5, 6]
+print(insertion_sort_desc(arr))
