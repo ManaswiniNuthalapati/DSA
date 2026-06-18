9f2c6ff4654d08d830be5d74eca4a3b1825c7b69
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-18 10:00:00 +0530

    Work done

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -25,3 +25,12 @@ def insertion_sort_desc(arr):
     return arr
 arr = [12, 11, 13, 5, 6]
 print(insertion_sort_desc(arr))
+
+# Check if Array is Sorted
+def is_sorted(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            return False
+    return True
+print(is_sorted([1, 2, 3, 4]))
+print(is_sorted([4, 2, 3, 1]))
