21755dc09cfecbff6bada27b173a4246e1665bbd
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-19 10:00:00 +0530

    Work done

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -86,3 +86,10 @@ print(arr)
 print(c)
 
 # Largest Number
+arr=[7,2,9,1,5]
+n=len(arr)
+for i in range(n-1):
+    for j in range(n-1-i):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+print(arr[-1])
