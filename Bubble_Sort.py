a302afc005f53977ee92216759237fda35a9b05e
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-19 10:00:00 +0530

    Work done

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -74,3 +74,13 @@ for i in range(n):
 print(arr)
 
 # Count Swaps
+arr=[5,1,4,2,8]
+n=len(arr)
+c=0
+for i in range(n-1):
+    for j in range(n-1-i):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+            c+=1
+print(arr)
+print(c)
