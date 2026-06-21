3119c2a4daa9c787ad64a2d4fbccc665ffea90ec
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-21 10:00:00 +0530

    Work done

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -113,3 +113,10 @@ for i in range(n-1):
 print(arr)
 
 # Sort Characters
+arr=list("python")
+n=len(arr)
+for i in range(n-1):
+    for j in range(n-1-i):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+print("".join(arr))
