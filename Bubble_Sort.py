9d507dc19d4a1e0fb2cc9e097d1f94cf646f3a6b
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-21 10:00:00 +0530

    Work done

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -104,3 +104,10 @@ for i in range(n-1):
 print(arr[0])
 
 # Sort Strings
+arr=["banana","apple","orange","grape"]
+n=len(arr)
+for i in range(n-1):
+    for j in range(n-1-i):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+print(arr)
