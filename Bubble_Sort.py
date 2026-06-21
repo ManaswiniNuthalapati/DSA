abbe1d634fd6c632f426f1288a7b9c027b5db531
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-21 10:00:00 +0530

    Work done

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -122,3 +122,10 @@ for i in range(n-1):
 print("".join(arr))
 
 # Sort Negative Numbers
+arr=[5,-2,9,-8,1]
+n=len(arr)
+for i in range(n-1):
+    for j in range(n-1-i):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+print(arr)
