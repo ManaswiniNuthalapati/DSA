2ec68a94eeca1ec1180377fc38f3bd132813318b
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-26 10:00:00 +0530

    Work done

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,1 +1,20 @@
-# Merge Sort Algorithm
+# Merge Sort
+'''
+Merge Sort is a Divide and Conquer sorting algorithm. 
+It divides the array into smaller parts, sorts those parts, and then merges them back together in sorted order.
+'''
+# Definition
+'''
+Merge Sort is a sorting algorithm that recursively divides an array into two halves 
+until each subarray contains one element, then merges the subarrays in sorted order.
+'''
+# Why is it called Divide and Conquer?
+'''
+It works in three steps:
+--> Divide
+    Divide the array into two halves.
+--> Conquer
+    Recursively sort each half.
+--> Combine
+    Merge the two sorted halves.
+'''
