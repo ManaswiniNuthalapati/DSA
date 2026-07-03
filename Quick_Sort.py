5261b8f26a4fc0eb065d60ea360d123d958c1c1e
Manaswini <nuthalapatimanaswini@gmail.com>
2026-07-03 10:00:00 +0530

    Work done

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -50,4 +50,23 @@ Reason - Recursion depth becomes n.
 '''
 
 # Quick Sort Lomuto Partition
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 
+def quickSort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quickSort(arr, low, pivot_index - 1)
+        quickSort(arr, pivot_index + 1, high)
+
+
+arr = [8, 3, 1, 7, 0, 10, 2]
+quickSort(arr, 0, len(arr) - 1)
+print(arr)
