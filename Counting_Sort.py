1ce8cd876f001ff80b5cd5cc41386d2e5bc8bc85
Manaswini <nuthalapatimanaswini@gmail.com>
2026-07-07 10:00:00 +0530

    Your commit message

--- Counting_Sort.py
+++ Counting_Sort.py
@@ -115,3 +115,24 @@ for i in reversed(arr):
 print(output)
 
 # Uncommon words from two Sentences
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        freq = {}
+        words1 = s1.split()
+        words2 = s2.split()
+        for word in words1:
+            if word in freq:
+                freq[word] += 1
+            else:
+                freq[word] = 1
+        for word in words2:
+            if word in freq:
+                freq[word] += 1
+            else:
+                freq[word] = 1
+        ans = []
+        for word in freq:
+            if freq[word] == 1:
+                ans.append(word)
+        return ans
+        
