cf41e9a7fec0f53fd72374fadbf7e2dcbfc8d58e
Manaswini <nuthalapatimanaswini@gmail.com>
2026-07-08 17:56:40 +0530

    file updated

--- Counting_Sort.py
+++ Counting_Sort.py
@@ -1,1 +1,14 @@
 # Sort Colors
+arr = [2,0,2,1,1,0]
+max_val=max(arr)
+count=[0]*(max_val+1)
+for num in arr:
+    count[num]+=1
+j=0
+for i in range(len(count)):
+    while count[i]>0:
+        arr[j]=i
+        j+=1
+        count[i]-=1
+print(arr)
+        
