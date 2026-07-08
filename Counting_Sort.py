3634e0e91d7d89859eb55036bcd660e6a88fce97
Manaswini <nuthalapatimanaswini@gmail.com>
2026-07-08 18:20:03 +0530

    file updated

--- Counting_Sort.py
+++ Counting_Sort.py
@@ -12,3 +12,22 @@ for i in range(len(count)):
         count[i]-=1
 print(arr)
         
+# Height Checker
+heights = [1,1,4,2,1,3]
+max_val=max(heights)
+count=[0]*(max_val+1)
+for num in heights:
+    count[num]+=1
+res=[]
+for i in range(len(count)):
+    while count[i]>0:
+        res.append(i)
+        count[i]-=1
+print(res)
+res=[1,1,1,2,3,4]
+count=0
+for i in range(len(heights)):
+    if heights[i]==res[i]:
+        count+=1
+print(count)
+        
