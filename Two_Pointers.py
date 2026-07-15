656db30fa258b5cc4cab20b9db6c14fdf58026ae
Manaswini <nuthalapatimanaswini@gmail.com>
2026-07-15 10:00:00 +0530

    Your commit message

--- Two_Pointers.py
+++ Two_Pointers.py
@@ -38,3 +38,41 @@ Both move left → right, but they do not always move together.
 Fast pointer → keeps scanning every element.
 Slow pointer → only moves when we find an element we want to keep.
 '''
+# When to use:
+'''
+different conditions. One pointer (slow) tracks the "result/write position," the other
+· Removing duplicates in-place
+· Removing a specific value in-place
+· Moving zeroes/elements to one side
+(fast ) scans ahead to find valid elements. This is common for in-place array modification.
+· Partitioning arrays (like in QuickSort's partition step
+'''
+
+# TYPE C: Two Pointers Across Two Different Sequences (Merge Pattern)
+'''
+Definition: Instead of one array with two pointers, you have two separate sorted
+arrays/lists, each with its own pointer, both starting at index 0. You compare the elements
+
+When to use:
+pointed to and advance the pointer of the smaller (or otherwise "losing") element.
+· Merging two sorted arrays/lists (as in Merge Sort's merge step)
+· Finding intersection/union of two sorted arrays
+· Comparing two sequences element by elemenT
+'''
+
+# Maximize and Minimize pair sum of array
+class Solution:
+    def minPairSum(self,nums):
+        nums.sort()
+        n=len(nums)
+        left=0
+        right=n-1
+        maxi=0
+        while left<right:
+            total=nums[left]+nums[right]
+            if total>maxi:
+                maxi=total
+            left+=1
+            right-=1
+        return maxi
+        
