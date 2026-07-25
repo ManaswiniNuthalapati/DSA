94c96ef07bffb5393a1b88c49a2578336cb2068b
Manaswini <nuthalapatimanaswini@gmail.com>
2026-07-25 10:00:00 +0530

    Your commit message

---
+++ Stack.py
@@ -0,0 +1,7 @@
+# Definition
+'''
+A Stack is a linear data structure that follows the LIFO principle - Last In, First Out.
+The last element inserted is the first one to be removed.
+Insertion and deletion happen only from one end, called the top.
+Real-life analogy: a stack of plates. You add a plate on top, and you remove from the top too - never from the bottom or middle.
+'''
