880a0e8a2998654450f2800de74b3f533369ec1d
Manaswini <nuthalapatimanaswini@gmail.com>
2026-07-25 10:00:00 +0530

    Your commit message

--- Stack.py
+++ Stack.py
@@ -5,3 +5,18 @@ The last element inserted is the first one to be removed.
 Insertion and deletion happen only from one end, called the top.
 Real-life analogy: a stack of plates. You add a plate on top, and you remove from the top too - never from the bottom or middle.
 '''
+
+# Core operations:
+'''
+Operation
+1. push(x)
+    Insert element (x) on top of the stack
+2. (pop())
+    Remove and return the top element
+3. (peek())/(top()
+    Return the top element without removing it
+4. (isEmpty()
+    Check if the stack has no elements
+5. isFull()
+    Check if the stack (fixed-size) has no room left
+'''
